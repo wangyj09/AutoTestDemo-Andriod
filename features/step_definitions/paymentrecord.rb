6f1f3e101c4bdf99e77ec959033d3d1bc56ec95c
WangYJ <wangyj@WangYJdeMacBook-Pro.local>
2019-05-16 18:51:02 +0800

    err

--- features/step_definitions/paymentrecord.rb
+++ features/step_definitions/paymentrecord.rb
@@ -10,7 +10,8 @@ end
     expect(actual).to eq(false)
 end
 当("点击为他人交费记录") do
-    driver.find_element(:id, "com.sitech.ac:id/myorder").click
+    #driver.find_element(:id, "com.sitech.ac:id/myorder").click
+    driver.find_element(:id, "123").click
 end
 
 那么("进入为他人交费记录页面") do
